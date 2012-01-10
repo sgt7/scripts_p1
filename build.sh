494c9169042bab4ef5fa4a41484650a2a9c00861
Chirayu Desai <chirayudesai1@gmail.com>
2012-01-10 16:41:45 +0530

    first commit

---
+++ build.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#
+# Script to build CM9 for Galaxy Tab (with Kernel)
+# 2012 Chirayu Desai 
+
+# Common defines
+txtrst='\e[0m'  # Color off
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yellow
+txtblu='\e[0;34m' # Blue
+
+echo -e "${txtblu}##########################################"
+echo -e "${txtblu}#                                        #"
+echo -e "${txtblu}#         GALAXYTAB BUILDSCRIPT          #"
+echo -e "${txtblu}#                                        #"
+echo -e "${txtblu}##########################################"
+echo -e "\r\n ${txtrst}"
+
+# Starting Timer
+START=$(date +%s)
+DEVICE="$1"
+BUILDTYPE="$2"
+THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+
+case "$DEVICE" in
+	clean)
+		make clean
+		cd kernel/samsung/p1
+		./build.sh clean
+		exit
+		;;
+	p1|P1)
+		TARGET=P1
+		;;
+	p1c|P1C)
+		TARGET=P1C
+		;;
+	p1l|P1L)
+		TARGET=P1L
+		;;
+	p1n|P1N)
+		TARGET=P1N
+		;;
+	*)
+		echo -e "${txtred}Usage: $0 device"
+		echo -e "Example: ./build.sh p1"
+		echo -e "Supported Devices: p1 p1c p1l p1n"
+		echo -e "This is needed only if you want to build kernel too ${txtrst}"
+		;;
+esac
+
+case "$BUILDTYPE" in
+	eng)
+		LUNCH=cm_galaxytab-eng
+		;;
+	userdebug)
+		LUNCH=cm_galaxytab-userdebug
+		;;
+	user)
+		LUNCH=cm_galaxytab-user
+		;;
+	*)
+		echo -e "${txtred} Choose a build type"
+		echo -e "Supported Buildtypes : eng userdebug user"
+esac
+
+if [ "$2" = "" ] ; then
+LUNCH=cm_galaxytab-userdebug
+fi
+
+# Check for Prebuilts
+		echo -e "${txtylw}Checking for Prebuilts...${txtrst}"
+if [ ! -e vendor/cm/proprietary/RomManager.apk ] || [ ! -e vendor/cm/proprietary/Term.apk ] || [ ! -e vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm3.so ]; then
+		echo -e "${txtred}Prebuilts not found, downloading now...${txtrst}"
+		cd vendor/cm
+		./get-prebuilts
+		cd ../..
+else
+		echo -e "${txtgrn}Prebuilts found.${txtrst}"
+fi
+
+# Setup build environment and start the build
+. build/envsetup.sh
+lunch $LUNCH
+
+# Kernel build
+if [ ! "$1" = "" ] ; then
+cd kernel/samsung/p1
+./build.sh $TARGET
+cp boot.img ../../../out/target/product/galaxytab
+fi
+
+# Android build
+make -j$THREADS bacon
+
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
---
+++ build_kernel.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+#
+# Script to build Galaxy Tab Kernel
+# 2012 Chirayu Desai 
+# This actually goes to kernel/samsung/p1
+
+# Common defines
+txtrst='\e[0m'  # Color off
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yellow
+txtblu='\e[0;34m' # Blue
+
+echo -e "${txtblu}##########################################"
+echo -e "${txtblu}#                                        #"
+echo -e "${txtblu}#      GALAXYTAB KERNEL BUILDSCRIPT      #"
+echo -e "${txtblu}#                                        #"
+echo -e "${txtblu}##########################################"
+echo -e "\r\n ${txtrst}"
+
+# Starting Timer
+START=$(date +%s)
+DEVICE="$1"
+THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+
+case "$DEVICE" in
+	clean)
+		make clean
+		exit
+		;;
+	p1|P1)
+		DEFCONFIG=p1_cm9_defconfig
+		;;
+	p1c|P1C)
+		DEFCONFIG=p1c_cm9_defconfig
+		;;
+	p1l|P1L)
+		DEFCONFIG=p1l_cm9_defconfig
+		;;
+	p1n|P1N)
+		DEFCONFIG=p1n_cm9_defconfig
+		;;
+	*)
+		echo -e "${txtred}Usage: $0 device"
+		echo -e "Example: ./build.sh p1"
+		echo -e "Supported Devices: p1 p1c p1l p1n ${txtrst}"
+		;;
+esac
+
+# The real build starts now
+if [ ! "$1" = "" ] ; then
+make -j$THREADS ARCH=arm $DEFCONFIG
+make -j$THREADS
+fi
+
+# Make it into a boot.img
+# ty nubecoder for this :)
+# defines
+KERNEL_DIR=`pwd`
+KERNEL_PATH="./arch/arm/boot/zImage"
+KERNEL_INITRD_DIR="../initramfs"
+KERNEL_INITRD_GIT="https://github.com/sgt7/p1000-initramfs-cm9.git"
+
+# Check if initramfs is present, if not, then clone it
+if [ ! -d $KERNEL_INITRD_DIR ]; then
+	cd ..
+	git clone $KERNEL_INITRD_GIT initramfs
+	cd $KERNEL_DIR
+fi
+
+# .git is huge!
+mv $KERNEL_INITRD_DIR/.git DONOTLOOKATME
+
+# The real build starts now
+if [ ! "$1" = "" ] ; then
+make -j$THREADS ARCH=arm $DEFCONFIG
+make -j$THREADS
+
+# Function
+function PACKAGE_BOOTIMG()
+{
+	if [ "$1" = "" ] || [ "$2" = "" ] ; then
+		ERROR_MSG="Error: PACKAGE_BOOTIMG - Missing args!"
+		return 2
+	fi
+	if [ ! -f "$1" ] ; then
+		ERROR_MSG="Error: PACKAGE_BOOTIMG - zImage does not exist!"
+		return 1
+	else
+		echo -e "${txtblu} Creating ramdisk.img"
+		./tools/mkbootfs $KERNEL_INITRD_DIR | ./tools/minigzip > ramdisk.img
+		if [ -f boot.img ] ; then
+			echo "removing old boot.img"
+			rm -f boot.img
+		fi
+		echo -e "${txtblu} Creating boot.img"
+		./tools/mkshbootimg.py boot.img ./arch/arm/boot/zImage ramdisk.img
+		echo -e "${txtblu} Cleaning up temp files:"
+		rm -f ramdisk.img
+		echo -e "${txtblu} Done!"
+		return 0
+	fi
+}
+
+# Main
+if [ ! "$1" = "" ] ; then
+PACKAGE_BOOTIMG "$KERNEL_PATH" "$KERNEL_INITRD_DIR"
+if [ $? != 0 ] ; then
+	echo -e "${txtred} $ERROR_MSG"
+else
+	echo -e "${txtblu} Boot.img created successfully...${txtrst}"
+fi
+fi
+
+# move it back just in case
+mv DONOTLOOKATME $KERNEL_INITRD_DIR/.git
+
+# The end!
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
